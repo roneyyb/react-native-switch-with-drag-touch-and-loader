824bf923ba89a8fdea849235fd9509828c7c95cd
roneyyb <bothra.rajat08@gmail.com>
2023-12-26 11:14:43 +0530

    Linting Fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-switch-with-drag-touch-and-loader';
+import { StyleSheet, View, Text } from "react-native";
+import { multiply } from "react-native-switch-with-drag-touch-and-loader";
 
 export default function App() {
 	const [result, setResult] = React.useState<number | undefined>();
@@ -20,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
+		alignItems: "center",
+		justifyContent: "center"
 	},
 	box: {
 		width: 60,
