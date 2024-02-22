ef5a8201008b86ef6982337d4ac75acc1990e452
roneyyb <bothra.rajat08@gmail.com>
2024-02-22 18:13:05 +0530

    UI FIXES

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, ToastAndroid, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import SwitchWithTouchAndDrag from "react-native-switch-with-drag-touch-and-loader";
 
@@ -16,7 +16,13 @@ export default function App() {
 			console.log(state, "promise");
 			if (state === "right" || state === "left") {
 				setTimeout(() => {
-					resolve("Done");
+					if (switchStateNormal === "left") {
+						reject("Done");
+						ToastAndroid.show("LOADING FAILED", 1000);
+					} else {
+						ToastAndroid.show("LOADING SUCCESS", 1000);
+						resolve("Done");
+					}
 				}, 500);
 			} else {
 				reject("");
@@ -31,12 +37,43 @@ export default function App() {
 				}
 			</Text>
 
-			<Text style={{ fontSize: 16 }}>SWITCH WITH LOADING</Text>
+			<Text style={{ fontSize: 16, fontWeight: "bold" }}>NORMAL SWITCH</Text>
+			<Text style={{ fontSize: 12, textAlign: "center" }}>
+				In this switch you have to provide switchType as normal and switchState
+				is internally handled and provided in switchChangeCallback
+			</Text>
+			<View style={{ marginTop: 10 }} />
+			<View style={{ flexDirection: "row", alignItems: "center" }}>
+				<Text>{"LOADING SUCCESS"}</Text>
+				<View style={{ marginLeft: 10 }} />
+				<SwitchWithTouchAndDrag
+					switchBackgroundColor={
+						switchStateNormal === "right" ? "#22733D" : "rgba(0, 0, 0,0.3);"
+					}
+					switchBorderColor={"rgba(255, 255, 255, 0.4)"}
+					pieceBackgroundColor="#FFFFFF"
+					switchBorderWidth={2}
+					pieceWidth={35}
+					pieceHeight={35}
+					switchHeight={35}
+					switchWidth={80}
+					switchBorderRadius={30}
+					initialSwitchState={"right"}
+					switchType={"normal"}
+					switchChangeCallback={(state: "right" | "left") => {
+						setSwitchStateNormal(state);
+					}}
+				/>
+			</View>
+			<View style={{ marginTop: 20 }} />
+			<Text style={{ fontSize: 16, fontWeight: "bold" }}>
+				SWITCH WITH LOADING
+			</Text>
 			<Text style={{ fontSize: 12 }}>
 				In this switch you have to provide switchType as loading and switchState
 				will always depend on outside switch state
 			</Text>
-			<View style={{ marginTop: 10 }} />
+			<View style={{ marginTop: 5 }} />
 			<View style={{ flexDirection: "row" }}>
 				<SwitchWithTouchAndDrag
 					switchBackgroundColor={
@@ -45,13 +82,13 @@ export default function App() {
 					switchBorderColor={"rgba(255, 255, 255, 0.4)"}
 					pieceBackgroundColor={"#FFFFFF"}
 					switchBorderWidth={2}
-					pieceWidth={40}
+					pieceWidth={37}
 					pieceHeight={40}
 					switchHeight={41}
 					switchWidth={90}
 					switchBorderRadius={50}
 					initialSwitchState={"right"}
-					activityIndicatorColor={"rgba(0, 0, 0,0.4);"}
+					activityIndicatorColor={"rgba(0, 0, 0,0.8);"}
 					switchType={"loading"}
 					switchChangeCallback={(state: "right" | "left") => {
 						setSwitchStateWithLoader(state);
@@ -72,29 +109,6 @@ export default function App() {
 					changeSwitchState={switchStateWithLoader}
 				/>
 			</View>
-			<View style={{ marginTop: 10 }} />
-
-			<Text style={{ fontSize: 16 }}>NORMAL SWITCH</Text>
-
-			<View style={{ marginTop: 10 }} />
-			<SwitchWithTouchAndDrag
-				switchBackgroundColor={
-					switchStateNormal === "right" ? "#22733D" : "rgba(0, 0, 0,0.3);"
-				}
-				switchBorderColor={"rgba(255, 255, 255, 0.4)"}
-				pieceBackgroundColor="#FFFFFF"
-				switchBorderWidth={2}
-				pieceWidth={35}
-				pieceHeight={35}
-				switchHeight={35}
-				switchWidth={80}
-				switchBorderRadius={30}
-				initialSwitchState={"right"}
-				switchType={"normal"}
-				switchChangeCallback={(state: "right" | "left") => {
-					setSwitchStateNormal(state);
-				}}
-			/>
 		</GestureHandlerRootView>
 	);
 }
